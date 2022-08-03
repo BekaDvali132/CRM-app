489a44295509503dee78265c1680c63f6cf36c9f
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2022-08-03 14:57:20 +0400

    heroku commit

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,48 @@
 const express = require("express");
-const colors = require('colors')
+const colors = require("colors");
 const dotenv = require("dotenv").config();
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-const {getMe} = require('./controllers/userController')
-const {protect} = require('./middleware/authMiddleware')
-const cors = require('cors')
-const path = require('path')
+const { getMe } = require("./controllers/userController");
+const { protect } = require("./middleware/authMiddleware");
+const cors = require("cors");
+const path = require("path");
+const compression = require('compression')
 
-connectDB()
+connectDB();
 
 const corsOption = {
-    origin: 'http://localhost:3000',
-}
+  origin: "http://localhost:3000",
+};
 
 const app = express();
 
-app.use(cors(corsOption))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(compression())
 
-app.use('/resources', express.static(path.join(__dirname, './resources')));
-app.use(`/resources`,express.static('resources'))
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/resources", express.static(path.join(__dirname, "./resources")));
+app.use(`/resources`, express.static("resources"));
 app.use("/api/clinics", require("./routes/clinicRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.get("/api/user/me", protect, getMe);
+
+// Serve frontend
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("not production");
+  });
+}
 
 app.listen(port, () => console.log(`server starter on port ${port}`));
